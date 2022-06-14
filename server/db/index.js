f3a5b40a4e0b6424269054f86fa7d92de5ffe87f
Emanuel Guevara <emanuelguevara@me.com>
2022-06-14 16:43:52 -0400

    finished defining database models

--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,24 @@
 //this is the access point for all things database related!
-const db = require("./db");
+const db = require('./db');
 
-const User = require("./models/User");
-const ShoppingSession = require("./models/ShoppingSession");
-const CartItem = require("./models/CartItem");
-const Product = require("./models/Product");
-const Inventory = require("./models/Inventory");
+const User = require('./models/User');
+const ShoppingSession = require('./models/ShoppingSession');
+const CartItem = require('./models/CartItem');
+const Product = require('./models/Product');
+const Inventory = require('./models/Inventory');
+const ProductCategory = require('./models/ProductCategory');
+const OrderItem = require('./models/OrderItem');
+const OrderDetails = require('./models/OrderDetails');
 
 //associations could go here!
 User.hasOne(ShoppingSession);
 ShoppingSession.hasMany(CartItem);
 Product.hasOne(CartItem);
 Inventory.belongsTo(Product);
+ProductCategory.hasMany(Product);
+Product.hasOne(OrderItem);
+OrderItem.belongsTo(OrderDetails);
+OrderDetails.belongsTo(User);
 
 module.exports = {
   db,
@@ -21,5 +28,8 @@ module.exports = {
     CartItem,
     Product,
     Inventory,
+    ProductCategory,
+    OrderItem,
+    OrderDetails,
   },
 };
