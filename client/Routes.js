521eff99d562cdb6a87bf7e6a7731733e4ff1d5e
Junbeom Chun <92chuck@gmail.com>
2022-06-15 18:02:49 -0400

    cart update

--- client/Routes.js
+++ client/Routes.js
@@ -1,23 +1,18 @@
-import React, { Component, Fragment } from "react";
-import { connect } from "react-redux";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
-import { Login, Signup } from "./components/AuthForm/AuthForm";
-import Home from "./components/Home/Home";
-import { me } from "./store";
-import AuthForm from "./components/AuthForm/AuthForm";
-import Cart from "./components/CartCheckout/Cart";
-import Checkout from "./components/CartCheckout/Checkout";
-import ConfirmationPage from "./components/CartCheckout/ConfirmationPage";
-import MyAccount from "./components/MyAccount/MyAccount";
-import ManagePage from "./components/ManagePage/ManagePage";
-import AllSnacks from "./components/SnacksPages/AllSnacks";
-//import { Cart, Checkout, ConfirmationPage } from "./components/CartCheckout";
-// import { AllSnacks, SingleSnacks } from "./components/SnacksPages";
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { Login, Signup } from './components/AuthForm/AuthForm';
+import Home from './components/Home/Home';
+import { me } from './store';
+import AuthForm from './components/AuthForm/AuthForm';
+import Cart from './components/CartCheckout/Cart';
+import Checkout from './components/CartCheckout/Checkout';
+import ConfirmationPage from './components/CartCheckout/ConfirmationPage';
+import MyAccount from './components/MyAccount/MyAccount';
+import ManagePage from './components/ManagePage/ManagePage';
+import AllSnacks from './components/SnacksPages/AllSnacks';
 // import { ErrorPage, MyAccount, ManagePage } from "./components";
 
-/**
- * COMPONENT
- */
 class Routes extends Component {
   componentDidMount() {
     this.props.loadInitialData();
@@ -30,22 +25,23 @@ class Routes extends Component {
       <div>
         {isLoggedIn ? (
           <Switch>
-            <Route exact path="/allsnacks" component={AllSnacks} />
-            <Route exact path="/cart" component={Cart} />
-            <Route exact path="/myaccount" component={MyAccount} />
-            <Route exact path="/managepage" component={ManagePage} />
-            <Route exact path="/" component={Home} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/allsnacks' component={AllSnacks} />
+            <Route exact path='/myaccount' component={MyAccount} />
+            <Route exact path='/managepage' component={ManagePage} />
+            <Route exact path='/cart' component={Cart} />
+            <Route exact path='/checkout' component={Checkout} />
           </Switch>
         ) : (
           <Switch>
-            <Route exact path="/allsnacks" component={AllSnacks} />
-            <Route exact path="/" component={Home} />
+            <Route exact path='/allsnacks' component={AllSnacks} />
+            <Route exact path='/' component={Home} />
 
-            <Route exact path="/login">
-              <AuthForm formName="login" />
+            <Route exact path='/login'>
+              <AuthForm formName='login' />
             </Route>
-            <Route exact path="/signup">
-              <AuthForm formName="signup" />
+            <Route exact path='/signup'>
+              <AuthForm formName='signup' />
             </Route>
           </Switch>
         )}
