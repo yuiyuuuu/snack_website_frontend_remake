7e7ba877b3c6998e0b601b748587ef1a799ceea1
Emanuel Guevara <emanuelguevara@me.com>
2022-06-16 18:34:43 -0400

    checkoutpage in progress

--- client/components/CartCheckout/Checkout.js
+++ client/components/CartCheckout/Checkout.js
@@ -1,14 +1,78 @@
 import React from 'react';
-import useStyles from './styles';
-import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const Checkout = () => {
+const useStyles = makeStyles({
+  root: {
+    marginTop: 80,
+    border: '2px solid red',
+    height: '75vh',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  columnCard: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'pink',
+  },
+  singleCard: {
+    height: '150px',
+    width: 'auto',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function Checkout() {
   const classes = useStyles();
-  return (
-    <>
-      <Typography className={classes.content}>CheckOut page</Typography>
-    </>
-  );
-};
 
-export default Checkout;
+  return (
+    <div className={classes.root}>
+      <div className={classes.columnCard}>
+        <Card className={classes.singleCard}>
+          <CardContent>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+            >
+              Your email
+            </Typography>
+            <Typography variant="body2" component="p">
+              Some information about the user
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardContent>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+            >
+              Your email
+            </Typography>
+            <Typography variant="body2" component="p">
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+      <Card>
+        <CardActions>
+          <Button size="small">Checkout</Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+}
