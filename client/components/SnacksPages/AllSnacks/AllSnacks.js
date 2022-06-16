7d23e0b34ccd574d592ac405d25da3a4cc7fc911
Yingson Yu <yingsonyu@gmail.com>
2022-06-16 17:45:49 -0500

    f

--- client/components/SnacksPages/AllSnacks/AllSnacks.js
+++ client/components/SnacksPages/AllSnacks/AllSnacks.js
@@ -1,12 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useStyles from "./AllSnackStyles";
 import { Container, Typography, Grid } from "@material-ui/core";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../../store/products";
 import SingleSnackComponent from "./SingleSnackComponent";
 import { fetchAUser } from "../../../store";
-import FilterImg from './FilterImg';
-
+import FilterImg from "./FilterImg";
 
 const AllSnacks = () => {
   const shuffle = (arr) => [...arr].sort(() => Math.random() - 0.5);
@@ -14,7 +13,7 @@ const AllSnacks = () => {
   const classes = useStyles();
   const { products } = useSelector((state) => state);
   const dispatch = useDispatch();
-  const [flavor, setFlavor] = useState('All');
+  const [flavor, setFlavor] = useState("All");
 
   useEffect(() => {
     dispatch(fetchProducts());
@@ -49,7 +48,7 @@ const AllSnacks = () => {
       <div>
         <main className={classes.root}>
           <div className={classes.toolbar} />
-          <Grid container justifyContent='flex-start' spacing={5}>
+          <Grid container justifyContent="flex-start" spacing={5}>
             <Grid
               item
               xs={12}
@@ -57,13 +56,13 @@ const AllSnacks = () => {
               md={4}
               lg={3}
               onClick={() => {
-                setFlavor('Salty');
+                setFlavor("Salty");
               }}
             >
               <FilterImg
-                flavor={'Salty'}
+                flavor={"Salty"}
                 img={
-                  'https://m.media-amazon.com/images/I/41DzPGQXiuL._AC_SY350_.jpg'
+                  "https://m.media-amazon.com/images/I/41DzPGQXiuL._AC_SY350_.jpg"
                 }
               />
             </Grid>
@@ -74,13 +73,13 @@ const AllSnacks = () => {
               md={4}
               lg={3}
               onClick={() => {
-                setFlavor('Sweet');
+                setFlavor("Sweet");
               }}
             >
               <FilterImg
-                flavor={'Sweet'}
+                flavor={"Sweet"}
                 img={
-                  'https://www.sweetflavorfl.com/img/my-sweet-flavor-logo-1618319663.jpg'
+                  "https://www.sweetflavorfl.com/img/my-sweet-flavor-logo-1618319663.jpg"
                 }
               />
             </Grid>
@@ -91,13 +90,13 @@ const AllSnacks = () => {
               md={4}
               lg={3}
               onClick={() => {
-                setFlavor('Healthy');
+                setFlavor("Healthy");
               }}
             >
               <FilterImg
-                flavor={'Healthy'}
+                flavor={"Healthy"}
                 img={
-                  'https://styles.redditmedia.com/t5_2scbp/styles/communityIcon_8pqszfejizl61.png'
+                  "https://styles.redditmedia.com/t5_2scbp/styles/communityIcon_8pqszfejizl61.png"
                 }
               />
             </Grid>
@@ -108,13 +107,13 @@ const AllSnacks = () => {
               md={4}
               lg={3}
               onClick={() => {
-                setFlavor('Refrigerated && Frozen');
+                setFlavor("Refrigerated && Frozen");
               }}
             >
               <FilterImg
-                flavor={'Refrigerated && Frozen'}
+                flavor={"Refrigerated && Frozen"}
                 img={
-                  'https://cdn.shopify.com/s/files/1/0294/2825/2771/collections/Category-Bars-Intro.png?v=1654034576'
+                  "https://cdn.shopify.com/s/files/1/0294/2825/2771/collections/Category-Bars-Intro.png?v=1654034576"
                 }
               />
             </Grid>
@@ -122,18 +121,18 @@ const AllSnacks = () => {
         </main>
       </div>
       <Typography
-        component='div'
-        variant='h1'
-        align='center'
-        onClick={() => setFlavor('All')}
+        component="div"
+        variant="h1"
+        align="center"
+        onClick={() => setFlavor("All")}
       >
         BROWSE ALL OF OUR SNACKS !
       </Typography>
       <div>
         <main className={classes.root}>
           <div className={classes.toolbar} />
-          <Grid container justifyContent='flex-start' spacing={4}>
-            {flavor === 'Salty'
+          <Grid container justifyContent="flex-start" spacing={4}>
+            {flavor === "Salty"
               ? saltyProducts.map((product) => {
                   return (
                     <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
@@ -141,7 +140,7 @@ const AllSnacks = () => {
                     </Grid>
                   );
                 })
-              : flavor === 'Sweet'
+              : flavor === "Sweet"
               ? sweetProducts.map((product) => {
                   return (
                     <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
@@ -149,7 +148,7 @@ const AllSnacks = () => {
                     </Grid>
                   );
                 })
-              : flavor === 'Healthy'
+              : flavor === "Healthy"
               ? healthyProducts.map((product) => {
                   return (
                     <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
@@ -157,7 +156,7 @@ const AllSnacks = () => {
                     </Grid>
                   );
                 })
-              : flavor === 'Refrigerated && Frozen'
+              : flavor === "Refrigerated && Frozen"
               ? frozenProducts.map((product) => {
                   return (
                     <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
