0e71f9e65e72de9edf245e8c23b8a0846410658e
Kenji Zhang <kenjizhang.1@gmail.com>
2022-06-20 13:44:40 -0700

    starting to set up backend routes for orders

--- server/api/orders.js
+++ server/api/orders.js
@@ -5,7 +5,36 @@ const {
 
 module.exports = router;
 
-// GET ALL ORDERS FOR USER(id)
+// ** CHECKOUT PAGE ** //
+// create new orderDetail and add all orderItems (need userId)
+// POST /api/orders/:id
+router.post('/:id', async (req, res, next) => {
+  try {
+    const newOrder = await OrderDetails.create({
+      userId: req.params.id,
+    });
+    res.json(newOrder);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// create a new orderItem for cartItem (need productId)
+// POST /api/orders/orderItem
+router.post('/:id', async (req, res, next) => {
+  try {
+    const newOrderItem = await OrderItem.create({
+      productId: req.params.id,
+    });
+    res.json(newOrderItem);
+  } catch (err) {
+    next(err);
+  }
+});
+// ** CHECKOUT PAGE ** //
+
+// ** ORDER HISTORY PAGE ** //
+// get all orders based on userId
 // GET /api/orders/:id
 router.get('/:id', async (req, res, next) => {
   try {
@@ -28,3 +57,4 @@ router.get('/:id', async (req, res, next) => {
     next(err);
   }
 });
+// ** ORDER HISTORY PAGE ** //
