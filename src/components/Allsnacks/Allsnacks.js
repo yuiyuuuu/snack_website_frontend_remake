6fd9c73528a17ae810d8af3f844ae69fba50c57b
Yingson Yu <yingsonyu@gmail.com>
2022-07-21 21:30:23 -0500

    allsnacks did more stuff

--- src/components/Allsnacks/Allsnacks.js
+++ src/components/Allsnacks/Allsnacks.js
@@ -1,36 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Allsnacks.css";
 import productData from "../../../script/productData";
 import SnackView from "./SnackView/SnackView";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../../store/Snacks";
 const left =
   "https://cdn-icons.flaticon.com/png/128/2985/premium/2985161.png?token=exp=1658385917~hmac=cbcc92465affce3974f896dee4fd8840";
 
-const right =
-  "https://cdn-icons.flaticon.com/png/128/2985/premium/2985179.png?token=exp=1658386163~hmac=9595b68f5075fd962c431633207d437b";
+const rightArrow =
+  "https://cdn-icons.flaticon.com/png/128/2985/premium/2985179.png?token=exp=1658449648~hmac=9c05aeaa10fae14449737f88789c5727";
 //for filtering, im thinking of a checkbox where you can select as many filters as you want
 //if user selects a filter, it gets pushed to an array
 //only render sections that is included in that array
 //if array is empty aka no filter, then render everything
 
 const Allsnacks = () => {
-  const counter = 0;
-  const counter2 = counter + 5;
-  const tensnacks = productData.slice(counter, counter2);
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state);
+  const salty = products.filter((item) => item.productCategoryId === 1);
+  const sweet = products.filter((item) => item.productCategoryId === 3);
+  const healthy = products.filter((item) => item.productCategoryId === 4);
+  const frozen = products.filter((item) => item.productCategoryId === 2);
+  console.log(frozen);
 
   const right = () => {
     counter = counter + 5;
     counter2 = counter2 + 5;
   };
 
-  const SnackCards = tensnacks.map((item) => (
+  const SnackCards = salty.map((item) => (
     <SnackView
       key={item.name}
       photoUrl={item.photoURL}
       title={item.name}
       description={item.desc}
       price={item.price}
+      snack={item}
     />
   ));
+
+  const SweetSnacks = sweet.map((item) => (
+    <SnackView
+      key={item.name}
+      photoUrl={item.photoURL}
+      title={item.name}
+      description={item.desc}
+      price={item.price}
+      snack={item}
+    />
+  ));
+
+  const HealthySnacks = healthy.map((item) => (
+    <SnackView
+      key={item.name}
+      photoUrl={item.photoURL}
+      title={item.name}
+      description={item.desc}
+      price={item.price}
+      snack={item}
+    />
+  ));
+
+  const FrozenSnacks = frozen.map((item) => (
+    <SnackView
+      key={item.name}
+      photoUrl={item.photoURL}
+      title={item.name}
+      description={item.desc}
+      price={item.price}
+      snack={item}
+    />
+  ));
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
 
   return (
     <div className='parent'>
@@ -51,19 +95,52 @@ const Allsnacks = () => {
           </div>
           <div className='checkbox-div'>
             <input type='checkbox' className='checkbox-input' />
-            Ice Cream
+            Frozen Snacks
           </div>
         </div>
       </div>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <div className='snack-title'>Sweet Snacks</div>
-        <div className='container'>
+        <div style={{ display: "flex", flexDirection: "row" }}>
           <div className='next-div'>
             <img src={left} className='leftandright' alt='lefticon' />
           </div>
-          {SnackCards}
+          <div className='container'>{SweetSnacks}</div>
+          <div className='next-div' onClick={() => right()}>
+            <img src={rightArrow} alt='righticon' className='leftandright' />
+          </div>
+        </div>
+
+        <div className='snack-title'>Healthy Snacks</div>
+        <div style={{ display: "flex", flexDirection: "row" }}>
           <div className='next-div'>
-            <img src={right} alt='righticon' className='leftandright' />
+            <img src={left} className='leftandright' alt='lefticon' />
+          </div>
+          <div className='container'>{HealthySnacks}</div>
+          <div className='next-div' onClick={() => right()}>
+            <img src={rightArrow} alt='righticon' className='leftandright' />
+          </div>
+        </div>
+
+        <div className='snack-title'>Salty Snacks</div>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div className='next-div'>
+            <img src={left} className='leftandright' alt='lefticon' />
+          </div>
+          <div className='container'>{SnackCards}</div>
+          <div className='next-div' onClick={() => right()}>
+            <img src={rightArrow} alt='righticon' className='leftandright' />
+          </div>
+        </div>
+
+        <div className='snack-title'>Frozen Snacks</div>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div className='next-div'>
+            <img src={left} className='leftandright' alt='lefticon' />
+          </div>
+          <div className='container'>{FrozenSnacks}</div>
+          <div className='next-div' onClick={() => right()}>
+            <img src={rightArrow} alt='righticon' className='leftandright' />
           </div>
         </div>
       </div>
