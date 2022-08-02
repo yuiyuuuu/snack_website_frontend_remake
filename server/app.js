fee30c96db31c7948c1c19af49902048c3dfef5c
yuiyuuuu <yingsonyu@gmail.com>
2022-08-01 22:01:09 -0500

    windows fix

--- server/app.js
+++ server/app.js
@@ -1,34 +1,35 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 module.exports = app;
+require("dotenv").config();
 
-const stripe = require('stripe')(
-  'sk_test_51L9yoAIILTUpIrN8axb1z8Jx5GFkY9TR9WBS9KJpZkWqWeHNDMxUogPOmTgA2DTsRAdyDy9GHWvKUVrCZx5ym7ZR00bsAEHoq2'
+const stripe = require("stripe")(
+  "sk_test_51L9yoAIILTUpIrN8axb1z8Jx5GFkY9TR9WBS9KJpZkWqWeHNDMxUogPOmTgA2DTsRAdyDy9GHWvKUVrCZx5ym7ZR00bsAEHoq2"
 );
 
 // if I am NOT in my production environment, I want access to the secrets.js file inside of my local machine (each dev should have one) --> development, test
-if (process.env.NODE_ENV !== 'production') require('./secrets');
+if (process.env.NODE_ENV !== "production") require("./secrets");
 
 // logging middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // body parsing middleware
 app.use(express.json());
 
 // auth and api routes
-app.use('/auth', require('./auth'));
-app.use('/api', require('./api'));
+app.use("/auth", require("./auth"));
+app.use("/api", require("./api"));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "..", "public/index.html"))
 );
 
 // static file-serving middleware
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.post('/payment', async (req, res) => {
+app.post("/payment", async (req, res) => {
   const { id, items } = req.body;
   const calculatePrice = (items) => {
     let totalPrice = 0;
@@ -45,19 +46,19 @@ app.post('/payment', async (req, res) => {
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: calculatePrice(items),
-      currency: 'USD',
+      currency: "USD",
       payment_method: id,
       confirm: true,
     });
 
     res.json({
-      message: 'Payment Successful',
+      message: "Payment Successful",
       success: true,
     });
   } catch (error) {
-    console.log('Error', error);
+    console.log("Error", error);
     res.json({
-      message: 'Payment failed',
+      message: "Payment failed",
       success: false,
     });
   }
@@ -65,7 +66,7 @@ app.post('/payment', async (req, res) => {
 // any remaining requests with an extension (.js, .css, etc.) send 404
 app.use((req, res, next) => {
   if (path.extname(req.path).length) {
-    const err = new Error('Not found');
+    const err = new Error("Not found");
     err.status = 404;
     next(err);
   } else {
@@ -74,13 +75,13 @@ app.use((req, res, next) => {
 });
 
 // sends index.html
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public/index.html"));
 });
 
 // error handling endware
 app.use((err, req, res, next) => {
   console.error(err);
   console.error(err.stack);
-  res.status(err.status || 500).send(err.message || 'Internal server error.');
+  res.status(err.status || 500).send(err.message || "Internal server error.");
 });
