72953aeae06c53ec7be0f6133cbd58f482cf9613
Devin <liudev777@gmail.com>
2022-08-03 19:01:01 -0500

    main screen stuff

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./home.css";
+import Navbar from "../Navbar/Navbar";
+
 
 const Home = () => {
-  return <div className='home'>
-    <a  href="/allsnacks">
-      All Snacks
-    </a> 
-  </div>;
+  return (
+    <div className='home'>
+      <Navbar/>
+      <div className="home--landingPage">
+        <a className="home--allsnacks" href="/allsnacks">
+          All Snacks
+        </a> 
+      </div>
+      <div>
+        hi
+      </div>
+    </div>
+  )
 };
 
 export default Home;
